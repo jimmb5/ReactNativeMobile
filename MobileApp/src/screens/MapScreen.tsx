cad94ab4db1fbd30d8d5e887cb1fdb27ecb43048
jmaunula <maunulajouni@gmail.com>
2026-04-16 15:19:37 +0300

    remove duplicate React import in MapScreen component

--- MobileApp/src/screens/MapScreen.tsx
+++ MobileApp/src/screens/MapScreen.tsx
@@ -1,6 +1,5 @@
 import { Text, View, TouchableOpacity } from "react-native"
 import React, { useRef } from "react"
-import React from "react"
 import { colors } from "../theme/colors"
 import { useEffect, useState } from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
